8ad07c2839ac3c4fa62e2b5cf5a2b7e629db74e9
iara <iarassantosdev404@gmail.com>
2025-09-25 15:05:40 -0300

    teste 102

--- 2022/vite.config.js
+++ 2022/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/2025/", // Configurado para funcionar em dominio.com/2025/
   plugins: [react()],
   build: {
     outDir: '../2025', // aqui você define a pasta de saída
