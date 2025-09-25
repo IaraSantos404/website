9bf35dba0989a3ef6960f99eb40ccbadc9299363
iara <iarassantosdev404@gmail.com>
2025-09-25 14:50:42 -0300

    teste 101

--- 2022/src/routes.jsx
+++ 2022/src/routes.jsx
@@ -4,7 +4,7 @@ import Palestrantes from "./pages/palestrantes/palestrantes"
 
 export default function AppRoutes(){
   return(
-    <BrowserRouter basename="/website/2025/">
+    <BrowserRouter basename="/2025/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/palestrantes" element={<Palestrantes />} />
